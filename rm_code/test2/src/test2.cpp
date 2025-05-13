cc2641356aead9addbc43ab8c232a549b8edd91a
FLumine87 <787249795@qq.com>
2025-05-13 16:55:00 +0800

    增加了pnp部分，可以进行部分调试

--- rm_code/test2/src/test2.cpp
+++ rm_code/test2/src/test2.cpp
@@ -21,7 +21,10 @@
 class ImgNode: public rclcpp::Node
 {
 public:
-    ImgNode(std::string name) : Node(name){
+    ImgNode(std::string name) : Node(name),
+    camera_matrix_(cv::Mat(3, 3, CV_64F, const_cast<double *>(camera_matrix.data())).clone()),
+    dist_coeffs_(cv::Mat(1, 5, CV_64F, const_cast<double *>(dist_coeffs.data())).clone())
+    {
         // 创建一个 image_transport 发布者
         publisher_i = this->create_publisher<sensor_msgs::msg::Image>("image_topic", 10);
 
@@ -37,11 +40,32 @@ public:
             RCLCPP_ERROR(this->get_logger(), "Failed to read image");
             rclcpp::shutdown();
         }
+
+        //pnp init
+        // Unit: m
+        constexpr double small_half_y = SMALL_ARMOR_WIDTH / 2.0 / 1000.0;
+        constexpr double small_half_z = SMALL_ARMOR_HEIGHT / 2.0 / 1000.0;
+        constexpr double large_half_y = LARGE_ARMOR_WIDTH / 2.0 / 1000.0;
+        constexpr double large_half_z = LARGE_ARMOR_HEIGHT / 2.0 / 1000.0;
+
+        // Start from bottom left in clockwise order
+        // Model coordinate: x forward, y left, z up
+        small_armor_points_.emplace_back(cv::Point3f(0, small_half_y, -small_half_z));
+        small_armor_points_.emplace_back(cv::Point3f(0, small_half_y, small_half_z));
+        small_armor_points_.emplace_back(cv::Point3f(0, -small_half_y, small_half_z));
+        small_armor_points_.emplace_back(cv::Point3f(0, -small_half_y, -small_half_z));
+
+        large_armor_points_.emplace_back(cv::Point3f(0, large_half_y, -large_half_z));
+        large_armor_points_.emplace_back(cv::Point3f(0, large_half_y, large_half_z));
+        large_armor_points_.emplace_back(cv::Point3f(0, -large_half_y, large_half_z));
+        large_armor_points_.emplace_back(cv::Point3f(0, -large_half_y, -large_half_z));
     }
 
     std::vector<rm_auto_aim::Light> lights;
+    rm_auto_aim::Armor armor;
     cv::Mat gbr_img;
     cv::Mat b_img;
+    float distance;
 
     //静态参数
     int binary_thres = 26;
@@ -73,6 +97,8 @@ private:
         // cv::destroyWindow("binary Image");
 
         lights = findLights(gbr_img, b_img);
+
+        pnpsolver();
 
         prit_lights();
 
@@ -225,6 +251,9 @@ private:
         cv::imshow("Lights Bounding Rectangles", display_img);
         cv::waitKey(0); // 等待用户按键
         cv::destroyWindow("Lights Bounding Rectangles");
+
+        //
+        RCLCPP_INFO(this->get_logger(), "  Distance: %f", distance);
     
         // 用户交互
         std::string input;
@@ -239,9 +268,54 @@ private:
         } while (input != "y");
     }
 
+    void pnpsolver(){
+        cv::Mat rvec, tvec;
+        rm_auto_aim::Armor armor(lights[0], lights[1]);
+        armor.type = rm_auto_aim::ArmorType::SMALL;
+        solvePnP(armor, rvec, tvec);
+        //
+        //暂时不知道是否要进行判断
+        distance =calculateDistanceToCenter(armor.center);
+    }
+
+    bool solvePnP(const rm_auto_aim::Armor & armor, cv::Mat & rvec, cv::Mat & tvec){
+        std::vector<cv::Point2f> image_armor_points;
+
+        // Fill in image points
+        image_armor_points.emplace_back(armor.left_light.bottom);
+        image_armor_points.emplace_back(armor.left_light.top);
+        image_armor_points.emplace_back(armor.right_light.top);
+        image_armor_points.emplace_back(armor.right_light.bottom);
+
+        // Solve pnp
+        auto object_points = armor.type == rm_auto_aim::ArmorType::SMALL ? small_armor_points_ : large_armor_points_;
+        return cv::solvePnP(
+            object_points, image_armor_points, camera_matrix_, dist_coeffs_, rvec, tvec, false,
+            cv::SOLVEPNP_IPPE);
+    }
+
+    float calculateDistanceToCenter(const cv::Point2f & image_point){
+        float cx = camera_matrix_.at<double>(0, 2);
+        float cy = camera_matrix_.at<double>(1, 2);
+        return cv::norm(image_point - cv::Point2f(cx, cy));
+    }
+
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_i;
     rclcpp::TimerBase::SharedPtr timer_i;
 
+    //pnp private
+    cv::Mat camera_matrix_;
+    cv::Mat dist_coeffs_;
+
+    // Unit: mm
+    static constexpr float SMALL_ARMOR_WIDTH = 132;
+    static constexpr float SMALL_ARMOR_HEIGHT = 57;
+    static constexpr float LARGE_ARMOR_WIDTH = 223;
+    static constexpr float LARGE_ARMOR_HEIGHT = 57;
+
+    // Four vertices of armor in 3d
+    std::vector<cv::Point3f> small_armor_points_;
+    std::vector<cv::Point3f> large_armor_points_;
 };
 
 int main(int argc, char **argv){
