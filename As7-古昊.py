9d39b120cddf7269dd7dcb4c2d5109eb1fa826aa
FLumine87 <787249795@qq.com>
2025-12-16 17:02:17 +0800

    数据结构As7

---
+++ As7-古昊.py
@@ -0,0 +1,40 @@
+# P1. Perform the Prim’s algorithm on the following graph, starting from vertex A. 
+
+# answer:
+# A → B (1)  
+# A,B → D (3)  
+# A,B,D → C (2)  
+# A,B,D,C → E (2)  
+# A,B,D,C,E → F (2)  
+# A,B,C,D,E,F → G (6)
+# The Total Weight of minimum spanning tree: 16
+
+#  P2. Perform the Kruskal’s algorithm on the following graph. 
+
+# answer:
+# A → B (1)
+# C → D (2)
+# D → E (2)
+# E → F (2)
+# B → D (3)
+# F → G (6)
+# The Total Weight of minimum spanning tree: 16
+
+# P3. Perform the Prim’s algorithm on the following graph, starting from vertex 1.
+
+# answer:
+# 1 → 5 (18)
+# 1,5 → 2 (15)
+# 1,2,5 → 4 (6)
+# 1,2,4,5 → 3 (14)
+# The Total Weight of minimum spanning tree: 53
+
+# P4. Perform the Kruskal’s algorithm on the following graph. 
+
+# answer:
+# 4 → 5 (6)
+# 4 → 3 (14)
+# 2 → 5 (15)
+# 1 → 5 (18)
+# The Total Weight of minimum spanning tree: 53
+
