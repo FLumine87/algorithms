5cbc5ddfe3c039257931fc574fec08c0af02cb05
FLumine87 <787249795@qq.com>
2025-12-17 10:59:25 +0800

    完成As6,7，放入tmp

--- tmp/As7-古昊.py
+++ tmp/As7-古昊.py
@@ -39,7 +39,22 @@
 # The Total Weight of minimum spanning tree: 53
 
 # P5. Given the following graph, run Dijkstra’s Algorithm to find the shortest path from 
-# A to H. P
+# A to H. 
+
+# answer:
+# turn 1: S = {A}; 
+# A → B (1)：dist[B] = 1，prev[B] = A
+# A → F (4)：dist[F] = 4，prev[F] = A
+# turn 2: S = {A, B};
+# B → F (2)：dist[F] = 3，prev[F] = B
+# B → C (6)：dist[C] = 7，prev[C] = B
+# B → H (1)：dist[H] = 2，prev[H] = B
+# turn 3: S = {A, B, H};
+# H → C (4)：dist[C] = 6，prev[C] = H
+# H → F (2)：remains dist[F] = 3，prev[F] = B
+# turn 4: S = {A, B, H, F};
+# F → C (3)：dist[C] = 6，prev[C] = F
+# shortest path: A → B → H (2)
 
 # P6. Find the shortest path from A to C. 
 
@@ -48,6 +63,24 @@
 
 # P7. Given the following graph, run Dijkstra’s Algorithm to find the shortest path from 
 # s to e. 
+
+# answer:
+# turn 1: S = {s};
+# s → a (1)：dist[a] = 1，prev[a] = s
+# s → b (5)：dist[b] = 5，prev[b] = s
+# turn 2: S = {s, a};
+# a → b (2)：dist[b] = 3，prev[b] = a
+# a → c (2)：dist[c] = 3，prev[c] = a
+# a → d (1)：dist[d] = 2，prev[d] = a
+# turn 3: S = {s, a, d};
+# d → e (2)：dist[e] = 4，prev[e] = d
+# turn 4: S = {s, a, d, e};
+# turn 5: S = {s, a, d, e, b};
+# b → d (2)：remains dist[d] = 2，prev[e] = a
+# turn 6: S = {s, a, d, e, b, c};
+# c → e (1)：remains dist[e] = 4，prev[e] = d
+# c → d (3)：remains dist[d] = 2，prev[d] = a
+# shortest path: s → a → d → e (4)
 
 # P8. Find the shortest path from s to c. 
 
